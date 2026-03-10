1edb8b5931cfbcbc809f8b5d2d39e63075d2f3ba
Ashlin_Binu <ashlinbinu53@gmail.com>
2026-03-11 00:25:24 +0530

    Time: 0 ms (100%), Space: 42.3 MB (56.42%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -2,16 +2,11 @@ class Solution {
     public int hammingDistance(int x, int y) {
         int count1=0;
         
-        int i = 1;
-        while(x>0 || y>0)
+        int d = x^y;
+        while(d!=0)
         {
-            if((x&1) != (y&1))
-            {
-                count1++;
-            }
-            x >>=1;
-            y >>=1;
-            i++;
+            d = d&(d-1);
+            count1++;
 
         }
        
