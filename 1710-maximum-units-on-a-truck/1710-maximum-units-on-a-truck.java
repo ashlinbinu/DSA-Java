634e78f23614e99306b9dfb3d11bdbd91d123600
Ashlin_Binu <ashlinbinu53@gmail.com>
2026-07-20 15:27:00 +0530

    Time: 2 ms (99.86%), Space: 47 MB (44.37%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,22 +1,38 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
-        int ans = 0;
-        for(int i = 0; i<boxTypes.length;i++)
+        // Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+        // int ans = 0;
+        // for(int i = 0; i<boxTypes.length;i++)
+        // {
+        //     if(boxTypes[i][0]<=truckSize)
+        //     {
+        //         ans+=boxTypes[i][0]*boxTypes[i][1];
+        //         truckSize-=boxTypes[i][0];
+        //     }
+        //     else if(boxTypes[i][0]>truckSize)
+        //     {
+        //         ans+=truckSize*boxTypes[i][1];
+        //         truckSize=0;
+        //     }
+        //     if(truckSize==0){return ans;}
+        //     if(i==boxTypes.length-1 && truckSize!=0){return ans;}
+
+        int[] count = new int[1001];//units per box
+        for(int[] box:boxTypes)
         {
-            if(boxTypes[i][0]<=truckSize)
-            {
-                ans+=boxTypes[i][0]*boxTypes[i][1];
-                truckSize-=boxTypes[i][0];
-            }
-            else if(boxTypes[i][0]>truckSize)
-            {
-                ans+=truckSize*boxTypes[i][1];
-                truckSize=0;
-            }
-            if(truckSize==0){return ans;}
-            if(i==boxTypes.length-1 && truckSize!=0){return ans;}
+            count[box[1]]+=box[0];
         }
-        return ans;
+        int total = 0;
+        for(int i = 1000;i>0;i--)
+        {
+            if(count[i]==0)continue;
+
+            int boxes = Math.min(count[i],truckSize);
+            total+=boxes*i;
+            truckSize-=boxes;
+            if(truckSize ==0) break;
+        }
+         return total;
+        }
+       
     }
-}
