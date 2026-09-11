061e4191a6ba33a0b278a20c68ede06e0c16b81b
Ashlin_Binu <ashlinbinu53@gmail.com>
2026-09-12 00:26:09 +0530

    Time: 5 ms (82.8%), Space: 45.9 MB (74.8%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+    
+    HashSet<Integer> set =new HashSet<>();
+        for(int i = 0; i< digits.length;i++)
+        {
+            for(int j = 0; j< digits.length;j++)
+            {
+                for(int k = 0; k<digits.length;k++)
+                {
+                    if(i!=j && j!=k && i!=k)
+                    {
+                        int num = digits[i]*100+digits[j]*10+digits[k];
+                        if(num%2 ==0 && digits[i]!=0)
+                        {
+                            set.add(num);
+                        }
+                    }
+                }
+            }
+        }
+return set.size();
+
+    }
+}
